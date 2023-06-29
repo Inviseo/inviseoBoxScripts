43b67ed03ac4b09b6afcb77ed18e5d5a658a5854
Vincent Agi <vincent@inviseo.fr>
2023-06-29 13:33:14 +0200

    fix install debian 12 or newer

--- downloadPyScada.sh
+++ downloadPyScada.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget https://github.com/vincent-inviseo/PyScada/archive/refs/heads/main.zip
+wget https://github.com/clavay/PyScada/archive/refs/heads/PyScadaMain.zip
 unzip main.zip
 cd PyScada-main
 sudo ./install_system.sh
