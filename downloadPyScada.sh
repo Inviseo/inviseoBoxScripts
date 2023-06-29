6abded0e669f134a8a3b898e97b43e078a88984e
Vincent Agi <vincent@inviseo.fr>
2023-06-29 10:39:24 +0200

    fix folder not found

--- downloadPyScada.sh
+++ downloadPyScada.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-wget -O ./PyScada https://github.com/vincent-inviseo/PyScada.git
-cd PyScada
-sudo ./install.sh
+wget -O ./PyScada https://github.com/vincent-inviseo/PyScada.git && cd PyScada && sudo ./install.sh
