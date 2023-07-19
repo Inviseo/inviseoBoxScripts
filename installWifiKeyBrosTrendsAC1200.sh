0b46746e46f2f3d9372a55d6a2091a26233b02c7
vincent-inviseo <132346203+vincent-inviseo@users.noreply.github.com>
2023-07-19 11:29:25 +0200

    Update installWifiKeyBrosTrendsAC1200.sh

--- installWifiKeyBrosTrendsAC1200.sh
+++ installWifiKeyBrosTrendsAC1200.sh
@@ -1,8 +1,1 @@
-cd
-rm -rf rtl88x2bu
-git clone https://github.com/cilynx/rtl88x2bu.git
-cd rtl88x2bu
-sudo make backup_rtlwifi
-make
-sudo make install
-sudo modprobe 88x2bu
+ sh -c 'wget deb.trendtechcn.com/install -O /tmp/install && sh /tmp/install'
